705f0d390aedf8eb17a7c2527f42145ba0a18595
Khrystyna <kpolish1dam1b@gmail.com>
2023-11-17 17:48:19 +0100

    Creacion tablas y conexion a base de datos

--- SQL-Server/MainWindow.xaml.cs
+++ SQL-Server/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace SQL_Server
 {
@@ -20,9 +22,15 @@ namespace SQL_Server
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection connection;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string miConexion = ConfigurationManager.ConnectionStrings["SQL_Server.Properties.Settings.ConnectionString"].ConnectionString;
+            connection = new SqlConnection(miConexion);
+
         }
     }
 }
